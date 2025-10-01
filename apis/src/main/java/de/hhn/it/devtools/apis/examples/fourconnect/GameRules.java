ac6e671b5395a2625ba1463a7ea0593462c4f330
mguelbasar <mguelbasar@stud.hs.heilbronn.de>
2025-10-01 20:30:36 +0200

    Methode wurde eingefügt

--- apis/src/main/java/de/hhn/it/devtools/apis/examples/fourconnect/GameRules.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/examples/fourconnect/GameRules.java
@@ -1,5 +1,30 @@
 package de.hhn.it.devtools.apis.examples.fourconnect;
 
-public class GameRules {
+public record GameRules(int rows, int columns) {
 
+    
+    public boolean checkWinner(char[][] board, char player) {
+        return false;
+        
+    }
+
+    
+    private boolean checkHorizontal(char[][] board, char player) {
+        return false;
+       
+    }
+
+    
+    private boolean checkVertical(char[][] board, char player) {
+        return false;
+     
+    }
+
+    
+    private boolean checkDiagonal(char[][] board, char player) {
+        return false;
+      
+    }
+
+  
 }
