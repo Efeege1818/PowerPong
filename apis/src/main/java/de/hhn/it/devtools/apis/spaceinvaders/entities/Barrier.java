5fffaac16319b6b1608e2e4d3e6d3cb834f32443
Ioannis Panagiotidis <Ipanagioti@stud.hs-heilbronn.de>
2025-10-03 19:11:09 +0200

    Added the Direction Record and added first Logic to Entity classes

--- apis/src/main/java/de/hhn/it/devtools/apis/spaceinvaders/entities/Barrier.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/spaceinvaders/entities/Barrier.java
@@ -1,4 +1,7 @@
 package de.hhn.it.devtools.apis.spaceinvaders.entities;
 
+/**
+ * Represents an unmoving Barrier in the SpaceInvader game.
+ */
 public interface Barrier extends Entity{
 }
--- apis/src/main/java/de/hhn/it/devtools/apis/spaceinvaders/entities/Projectile.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/spaceinvaders/entities/Projectile.java
@@ -1,4 +1,7 @@
 package de.hhn.it.devtools.apis.spaceinvaders.entities;
 
+/**
+ * Represents the Projectiles in the SpaceInvaders game which can be shot by Ships or Aliens.
+ */
 public interface Projectile extends Entity{
 }
