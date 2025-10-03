07103b1d8d7b358f8126d2b05ce89a2a4fdf549f
Ioannis Panagiotidis <Ipanagioti@stud.hs-heilbronn.de>
2025-10-03 19:31:53 +0200

    added a static direction to all Aliens

--- apis/src/main/java/de/hhn/it/devtools/apis/spaceinvaders/entities/Alien.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/spaceinvaders/entities/Alien.java
@@ -1,9 +1,16 @@
 package de.hhn.it.devtools.apis.spaceinvaders.entities;
 
+import de.hhn.it.devtools.apis.spaceinvaders.Direction;
+
 /**
  * Aliens Represent the Enemies in the SpaceInvaders game.
  */
 public interface Alien extends Entity {
+
+  /**
+   * All Aliens move in the same direction which can be changed.
+   */
+  public static Direction alienDirection = Direction.RIGHT;
 
   /**
    * Creates a Projectile which will travel downwards.
