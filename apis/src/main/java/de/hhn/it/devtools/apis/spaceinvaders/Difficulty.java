e7fef55380e5f76933943c3e96ba982012f72198
leonard schell <lschell@stud.hs-heilbronn.de>
2025-10-03 19:05:44 +0200

    updated classes

---
+++ apis/src/main/java/de/hhn/it/devtools/apis/spaceinvaders/Difficulty.java
@@ -0,0 +1,7 @@
+package de.hhn.it.devtools.apis.spaceinvaders;
+/**
+ * Difficulty levels the game can have
+ */
+public enum Difficulty {
+  EASY, NORMAL, HARD
+}
--- apis/src/main/java/de/hhn/it/devtools/apis/spaceinvaders/GameConfiguration.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/spaceinvaders/GameConfiguration.java
@@ -1,4 +1,11 @@
 package de.hhn.it.devtools.apis.spaceinvaders;
-
-public record GameConfiguration() {
+/**
+ * Configures a Space Invaders game.
+ *
+ * @param numberOfBarriers sets the number of barriers on the field
+ * @param difficulty sets the difficulty of the game
+ */
+public record GameConfiguration(int numberOfBarriers, Difficulty difficulty) {
+    private static final org.slf4j.Logger logger =
+            org.slf4j.LoggerFactory.getLogger(GameConfiguration.class);
 }
