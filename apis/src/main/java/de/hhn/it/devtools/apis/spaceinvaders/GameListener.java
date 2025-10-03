b5c0206d2a9221e9c8ffb23827255b27c1bc1f09
Marcel Kraft <mkraft3@stud.hs-heilbronn.de>
2025-10-03 19:24:07 +0200

    update GameListener

--- apis/src/main/java/de/hhn/it/devtools/apis/spaceinvaders/GameListener.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/spaceinvaders/GameListener.java
@@ -1,4 +1,40 @@
 package de.hhn.it.devtools.apis.spaceinvaders;
 
+/**
+ * Models the capabilities of a GameListener.
+ */
 public interface GameListener {
+
+  /**
+   * Informs the listener that the field has changed.
+   *
+   * @param field field with the actual position of Entities elements
+   */
+  void updateBoard(Field field);
+
+  /**
+   * Informs the listener that the game state has changed.
+   *
+   * @param gameState new state of the game
+   */
+  void changedGameState(GameState gameState);
+
+  /**
+   * Informs the user that the game has ended.
+   */
+  void gameEnded();
+
+  /**
+   * Informs the listener that the score has changed.
+   *
+   * @param score new score value
+   */
+  void updateScore(int score);
+
+  /**
+   * Informs about a newly accepted game configuration.
+   *
+   * @param configuration new game configuration
+   */
+  void updateGameConfiguration(GameConfiguration configuration);
 }
