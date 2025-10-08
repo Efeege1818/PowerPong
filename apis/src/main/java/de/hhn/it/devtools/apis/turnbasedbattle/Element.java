dbe824f719f6aa46dba65728405633a7dfaad79e
Julian Clausing <jclausing@stud.hs-heilbronn.de>
2025-10-08 16:13:39 +0200

    added Element enum

---
+++ apis/src/main/java/de/hhn/it/devtools/apis/turnbasedbattle/Element.java
@@ -0,0 +1,27 @@
+package de.hhn.it.devtools.apis.turnbasedbattle;
+
+/**
+ * Enum to represent different elemental types of monsters and moves.
+ */
+public enum Element {
+  /**
+   * The object has the normal element.
+   * Normal has no special traits with other elements.
+   */
+  NORMAL,
+  /**
+   * The object has the fire element.
+   * Fire is effective against grass.
+   */
+  FIRE,
+  /**
+   * The object has the water element.
+   * Water is effective against fire.
+   */
+  WATER,
+  /**
+   * The object has the grass element.
+   * Grass is effective against water.
+   */
+  GRASS
+}
