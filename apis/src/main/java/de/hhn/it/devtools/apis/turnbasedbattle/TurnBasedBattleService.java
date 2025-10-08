f53e7f927241fa204d1afcb64c957c208bdb40db
Artur Kuschnir <artur.kuschnir24@gmail.com>
2025-10-08 17:47:44 +0200

    added Demo program and added listener methods to Service

--- apis/src/main/java/de/hhn/it/devtools/apis/turnbasedbattle/TurnBasedBattleService.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/turnbasedbattle/TurnBasedBattleService.java
@@ -22,5 +22,20 @@ public interface TurnBasedBattleService {
      */
     void end() throws IllegalStateException;
 
+    /**
+     * Adds a listener for game updates. Each listener can only be added once.
+     *
+     * @param listener listener to be added
+     * @return true if the listener could be added. Otherwise, false.
+     */
+    boolean addListener(TurnBasedBattleListener listener);
+
+    /**
+     * Removes a listener from the list of listeners.
+     *
+     * @param listener listener to be removed.
+     * @return true if the listener could be removed. Otherwise, false.
+     */
+    boolean removeListener(TurnBasedBattleListener listener);
 
 }
---
+++ apis/src/test/java/de/hhn/it/devtools/apis/turnbasedbattle/DemoTurnBasedBattleUsage.java
@@ -0,0 +1,10 @@
+package de.hhn.it.devtools.apis.turnbasedbattle;
+
+public class DemoTurnBasedBattleUsage {
+
+    public static void main(String[] args) {
+        TurnBasedBattleService service = null;
+
+        TurnBasedBattleListener listener = null;
+    }
+}
