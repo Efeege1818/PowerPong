d3cb265215a8d65df74f32620613d0d72f466023
mguelbasar <mguelbasar@stud.hs.heilbronn.de>
2025-10-11 17:54:01 +0200

    Game Rules Interface.

--- apis/src/main/java/de/hhn/it/devtools/apis/examples/fourconnect/GameRules.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/examples/fourconnect/GameRules.java
@@ -1,39 +1,12 @@
 package de.hhn.it.devtools.apis.examples.fourconnect;
 
-public class GameRules {
+public interface GameRules {
 
-    private final int rows;
-    private final int columns;
+    // Über das Spielfeld und den Spieler den Gewinner überprüfen
+    boolean checkWinner(Player[][] board, Player player);
 
-    public GameRules(int rows, int columns) {
-        this.rows = rows;
-        this.columns = columns;
-    }
-
-    
-    public boolean checkWinner(Player[][] board, Player player) {
-        return checkHorizontal(board, player)
-            || checkVertical(board, player)
-            || checkDiagonal(board, player);
-    }
-
-    
-    private boolean checkHorizontal(Player[][] board, Player player) {
-       
-        return false;
-    }
-
-    
-    private boolean checkVertical(Player[][] board, Player player) {
-      
-        return false;
-    }
-
-   
-    private boolean checkDiagonal(Player[][] board, Player player) {
-      
-        return false;
-    }
-
-    
+    // Überprüft, ob der Spieler 4 Steine in einer Reihe hat
+    boolean checkHorizontal(Player[][] board, Player player);
+    boolean checkVertical(Player[][] board, Player player);
+    boolean checkDiagonal(Player[][] board, Player player);
 }
