49208f6d0defaf9c6c89d7eb8f5c2473f95e5538
Stefan Arsenovici <arsenovici@stud.hs-heilbronn.de>
2025-10-14 11:19:45 +0200

    fixing some style

--- apis/src/main/java/de/hhn/it/devtools/apis/towerDefenseApis/Enemy.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/towerDefenseApis/Enemy.java
@@ -1,18 +1,21 @@
 package de.hhn.it.devtools.apis.towerDefenseApis;
 
 public interface Enemy {
-    int health();
-    int speed();
-    Path path();
+	int health();
 
-    /**
-     * Reduces the health of the enemy by the given amount and markes the enemy as dead,
-     * if the health has reached zero.
-     * @param amount the amount of health the enemy's health should be reduced by
-     * @return true if the enemy dies due to the health reduction
-     * @throws IllegalArgumentException if the amount is negative
-     */
-    public boolean getDamage(int amount);
+	int speed();
 
-    public Coordinates move();
+	Path path();
+
+	/**
+	 * Reduces the health of the enemy by the given amount and markes the enemy as dead,
+	 * if the health has reached zero.
+	 *
+	 * @param amount the amount of health the enemy's health should be reduced by
+	 * @return true if the enemy dies due to the health reduction
+	 * @throws IllegalArgumentException if the amount is negative
+	 */
+	public boolean getDamage(int amount);
+
+	public Coordinates move();
 }
