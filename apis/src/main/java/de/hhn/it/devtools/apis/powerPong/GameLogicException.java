73fb949b27dd324f39c182c8d83809dfca6d1fe3
hnasrullah <hnasrullah@stud.hs-heilbronn.de>
2025-10-20 00:10:39 +0200

    GameLogicException hinzugefügt

---
+++ apis/src/main/java/de/hhn/it/devtools/apis/powerPong/GameLogicException.java
@@ -0,0 +1,15 @@
+package de.hhn.it.devtools.apis.powerPong;
+
+/**
+ * A custom exception for errors that occur within the game logic
+ * (e.g., an invalid game state).
+ */
+public class GameLogicException extends Exception {
+    public GameLogicException(String message) {
+        super(message);
+    }
+
+    public GameLogicException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
