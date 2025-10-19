ef68d5a92de0e687ea61b6d77a0ae5867cc2d964
Amir Abu-Ghazaleh <aabughazal@stud.hs-heilbronn.de>
2025-10-19 23:17:23 +0200

    Enum GameMode erstellt

---
+++ apis/src/main/java/de/hhn/it/devtools/apis/powerPong/GameMode.java
@@ -0,0 +1,26 @@
+package de.hhn.it.devtools.apis.powerPong;
+
+/**
+ * Defines the available game modes.
+ */
+public enum GameMode {
+    /**
+     * Player vs. Player without power-ups.
+     */
+    CLASSIC_DUEL,
+
+    /**
+     * Player vs. Player with power-ups (must-have).
+     */
+    POWERUP_DUEL,
+
+    /**
+     * Player vs. AI (nice-to-have).
+     */
+    PLAYER_VS_AI,
+
+    /**
+     * Endless mode against AI (nice-to-have).
+     */
+    SURVIVAL_CHALLENGE
+}
