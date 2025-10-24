eabc3a996b631e4e2e14cd163a6f973f97a66318
Julian Clausing <jclausing@stud.hs-heilbronn.de>
2025-10-24 20:09:56 +0200

    added maxHp and currentHp to Monster

--- apis/src/main/java/de/hhn/it/devtools/apis/turnbasedbattle/Monster.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/turnbasedbattle/Monster.java
@@ -3,12 +3,13 @@ package de.hhn.it.devtools.apis.turnbasedbattle;
 /**
  * Record that allows creation of a new Monster with fully customizable stats.
  *
- * @param hp amount of health the monster has
+ * @param maxHp maximum amount of health the monster has
+ * @param currentHp current amount of health the monster has
  * @param attack increases damage dealt
  * @param defense reduces damage taken
  * @param evasionChance chance to not take any damage
  * @param critChance chance to inflict double damage
  * @param element element of the monster
  */
-public record Monster(int hp, int attack, int defense, double evasionChance, double critChance, Element element) {
+public record Monster(int maxHp,int currentHp, int attack, int defense, double evasionChance, double critChance, Element element) {
 }
