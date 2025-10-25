c95c896c6bf0521936f2d25ac14ec61a10043bb3
Artur Kuschnir <artur.kuschnir24@gmail.com>
2025-10-25 13:38:49 +0200

    updated move record

--- apis/src/main/java/de/hhn/it/devtools/apis/turnbasedbattle/Move.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/turnbasedbattle/Move.java
@@ -1,11 +1,11 @@
 package de.hhn.it.devtools.apis.turnbasedbattle;
 
 /**
- * Allows creation of new moves with custom effects
+ * Allows creation of new moves with custom effects.
  *
- * @param target player targeted by the move
- * @param amount number by which the selected stat will be changed
+ * @param type the type of the move.
+ * @param amount number by which the selected stat will be changed.
  * @param stat the stat that will be influenced by this move e.g. hp, attack, critChance,...
  */
-public record Move(Player target, int amount, String stat) {
+public record Move(MoveType type, int amount, String stat) {
 }
