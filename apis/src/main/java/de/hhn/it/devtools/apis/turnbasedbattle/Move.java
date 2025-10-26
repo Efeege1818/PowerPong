ba0768102da55e256fb28dd40a6869a512057972
Artur Kuschnir <artur.kuschnir24@gmail.com>
2025-10-26 08:56:24 +0100

    move record more parameters

--- apis/src/main/java/de/hhn/it/devtools/apis/turnbasedbattle/Move.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/turnbasedbattle/Move.java
@@ -6,6 +6,10 @@ package de.hhn.it.devtools.apis.turnbasedbattle;
  * @param type the type of the move.
  * @param amount number by which the selected stat will be changed.
  * @param stat the stat that will be influenced by this move e.g. hp, attack, critChance,...
+ * @param duration the number of turns the move will last.
+ * @param cooldown the number of turns the move will be on cooldown.
+ * @param isSpecial indicates if the move is special.
+ * @param description a description of the move.
  */
-public record Move(MoveType type, int amount, String stat) {
+public record Move(MoveType type, int amount, String stat, int duration, int cooldown, boolean isSpecial, String description) {
 }
