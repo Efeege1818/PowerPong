8d00809eabf39a137097880a7ac0938e3e288489
Luis Mißbach <lmissbach@stud.hs-heilbronn.de>
2025-11-01 17:47:04 +0100

    Fixed Checkstyle for WaveGenerator Interface

--- components/src/main/java/de/hhn/it/devtools/components/towerDefenseComponents/WaveGenerator.java
+++ components/src/main/java/de/hhn/it/devtools/components/towerDefenseComponents/WaveGenerator.java
@@ -2,25 +2,28 @@ package de.hhn.it.devtools.components.towerDefenseComponents;
 
 import java.util.Map;
 
+/**
+ * Generator that generates the subsequent enemy waves.
+ */
 public interface WaveGenerator {
-	// TODO Methoden adden
 
+  // TODO: Methoden adden
 
-	/**
-	 * Calculates the total enemy power for a given wave.
-	 *
-	 * @param wave the wave number to calculate the power for
-	 * @return the corresponding amount of enemy power
-	 * @throws IllegalArgumentException if wave number is less than or equal to zero
-	 */
-	int getEnemyPower(int wave, int multiplier);
+  /**
+   * Calculates the total enemy power for a given wave.
+   *
+   * @param wave the wave number to calculate the power for
+   * @return the corresponding amount of enemy power
+   * @throws IllegalArgumentException if wave number is less than or equal to zero
+   */
+  int getEnemyPower(int wave, int multiplier);
 
-	/**
-	 * Generates a wave of enemies with the enemyPower
-	 *
-	 * @param enemyPower will determine how strong the wave will be
-	 * @return Map filled with enemies and their id
-	 * @throws IllegalArgumentException if enemyPower number is less than or equal to zero
-	 */
-	Map<Integer, Enemy> generateWave(int enemyPower);
+  /**
+   * Generates a wave of enemies with the enemyPower.
+   *
+   * @param enemyPower will determine how strong the wave will be
+   * @return Map filled with enemies and their id
+   * @throws IllegalArgumentException if enemyPower number is less than or equal to zero
+   */
+  Map<Integer, Enemy> generateWave(int enemyPower);
 }
