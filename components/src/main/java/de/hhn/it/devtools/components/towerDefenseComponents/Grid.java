647f9fb9aac40c3290b3c360375236320b8ee0cb
Luis Mißbach <lmissbach@stud.hs-heilbronn.de>
2025-11-01 17:40:04 +0100

    Cleand Up Grid Interface

--- components/src/main/java/de/hhn/it/devtools/components/towerDefenseComponents/Grid.java
+++ components/src/main/java/de/hhn/it/devtools/components/towerDefenseComponents/Grid.java
@@ -2,20 +2,23 @@ package de.hhn.it.devtools.components.towerDefenseComponents;
 
 import de.hhn.it.devtools.apis.towerDefenseApis.Direction;
 
+/**
+ * Grid on wich the Path is generated and the Towers are placed.
+ */
 public interface Grid {
 
-    /**
-     * Generates a new Map and saves it as a two-dimensional array
-     *
-     * @param size the length and height of the map
-     */
-    public void generateGrid(int size);
+  /**
+   * Generates a new Map and saves it as a two-dimensional array.
+   *
+   * @param size the length and height of the map
+   */
+  public void generateGrid(int size);
 
-    /**
-     * Provides a copy of the underlying Data Structure that represents the enemy path
-     *
-     * @return the grid of Directions as a two-dimensional array
-     */
-    public Direction[][] getGrid();
+  /**
+   * Provides a copy of the underlying Data Structure that represents the enemy path.
+   *
+   * @return the grid of Directions as a two-dimensional array
+   */
+  public Direction[][] getGrid();
 
 }
