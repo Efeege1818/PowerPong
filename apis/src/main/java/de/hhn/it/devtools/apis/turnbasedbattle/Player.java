fb48f25442abad0d444c0048468e0ff2a0a3d87e
JulianClausing <jclausing@stud.hs-heilbronn.de>
2025-11-03 09:01:37 +0100

    converted Player to record

--- apis/src/main/java/de/hhn/it/devtools/apis/turnbasedbattle/Player.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/turnbasedbattle/Player.java
@@ -1,67 +1,13 @@
 package de.hhn.it.devtools.apis.turnbasedbattle;
 
 /**
- * handles inputs and creates monster objects.
+ * Record to allow creation of Player object.
+ *
+ * @param playerId id of the player.
+ * @param monster monster assigned to this player.
+ * @param score amount of rounds this player has won.
  */
-// TODO: Maybe change to record
-// Player only has a monster and a player ID and no methods,
-// methods are in BattleManager or service
-public class Player {
+public record Player(int playerId, Monster monster, int score) {
     private static final org.slf4j.Logger logger =
             org.slf4j.LoggerFactory.getLogger(Player.class);
-
-    // Fields
-    private int playerID; // Player1 oder Player 2
-    private Monster monster;
-
-    // Constructor
-    public Player(int playerID, Monster monster) {
-
-    }
-    /**
-     * Assigns a monster to this player.
-     * @param monster
-     */
-    public void assignMonster(Monster monster) {
-
-    }
-
-    /**
-     * Orders the Monster what to do.
-     */
-    public void commandMonster(){
-        //call Move Methods in monster class based on the input of the player
-    }
-
-    /**
-     * Returns this player's ID ("Player 1" or "Player 2").
-     *
-     * @return player ID.
-     */
-    public int getPlayerId() {
-        return 0;  //TODO: replace Placeholder value
-    }
-
-    /**
-     * Checks if this player's monster is still able to fight.
-     *
-     * @return true if monster has remaining HP.
-     */
-    public boolean isMonsterAlive(Monster monster){
-        return false;
-    }
-
-
-    public Monster getMonster() {
-        return monster;
-    }
-
-    /**
-     * Returns current health value of the monster that the player has.
-     *
-     * @return current hp of monster.
-     */
-    public int getMonsterCurrentHp() {
-        return monster.currentHp();
-    }
 }
