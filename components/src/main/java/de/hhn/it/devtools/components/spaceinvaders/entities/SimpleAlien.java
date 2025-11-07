89940fe61e454b238282f1487d3e3e3e86a73271
Ioannis Panagiotidis <Ipanagioti@stud.hs-heilbronn.de>
2025-11-07 17:29:38 +0100

    working on SimpleAlien

--- components/src/main/java/de/hhn/it/devtools/components/spaceinvaders/entities/SimpleAlien.java
+++ components/src/main/java/de/hhn/it/devtools/components/spaceinvaders/entities/SimpleAlien.java
@@ -1,4 +1,35 @@
 package de.hhn.it.devtools.components.spaceinvaders.entities;
 
+import de.hhn.it.devtools.apis.spaceinvaders.Coordinate;
+import de.hhn.it.devtools.apis.spaceinvaders.entities.AlienType;
+
+import java.util.ArrayList;
+
 public class SimpleAlien {
+  private static int X = 10;
+  static Integer alienIdCounter;
+  Coordinate coordinate;
+  Integer hitPoints;
+  AlienType alienType;
+  Integer alienId;
+  ArrayList<Coordinate> hitbox;
+
+  public SimpleAlien(Coordinate coordinate, AlienType alienType) {
+    this.coordinate = coordinate;
+    hitbox = drawHitbox(X);
+    this.alienType = alienType;
+    this.alienId = alienIdCounter++;
+    hitPoints = 3;
+  }
+
+  private ArrayList<Coordinate> drawHitbox(int x) {
+    ArrayList<Coordinate> coords = new ArrayList<>();
+    int j = 0;
+    for (int i = 0; i < x; i++) {
+      for (j = 0; j < x; j++) {
+        coords.add(new Coordinate(coordinate.x() + i, coordinate.y() + j));
+      }
+    }
+    return coords;
+  }
 }
--- components/src/main/java/de/hhn/it/devtools/components/spaceinvaders/entities/SimpleShip.java
+++ components/src/main/java/de/hhn/it/devtools/components/spaceinvaders/entities/SimpleShip.java
@@ -3,4 +3,5 @@ package de.hhn.it.devtools.components.spaceinvaders.entities;
 import de.hhn.it.devtools.apis.spaceinvaders.entities.Ship;
 
 public class SimpleShip {
+
 }
