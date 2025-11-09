54a22851ebf0a9057a0f7077ceb1d563f1b7d1ad
Luis Mißbach <lmissbach@stud.hs-heilbronn.de>
2025-11-09 19:16:12 +0100

    added money Parameter to enemy

--- apis/src/main/java/de/hhn/it/devtools/apis/towerdefenseapi/Enemy.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/towerdefenseapi/Enemy.java
@@ -3,6 +3,8 @@ package de.hhn.it.devtools.apis.towerdefenseapi;
 
 // TODO: verify throwable
 
+import java.util.NoSuchElementException;
+
 /**
  * Represents an enemy in the Tower Defense game.
  *
@@ -18,13 +20,15 @@ package de.hhn.it.devtools.apis.towerdefenseapi;
  * @param speed the Speed of this Enemy. A faster enemy has a higher speed value
  * @param health the current Health of this enemy
  * @param damage the amount by witch this enemy reduces the players health, if it comes through
+ * @param money the amount of money awarded to the player if this enemy is defeated
  */
 public record Enemy(int id,
                     Coordinates coordinates,
                     EnemyType type,
                     int speed,
                     int health,
-                    int damage
+                    int damage,
+                    int money
 ) {
 
   /**
@@ -32,9 +36,9 @@ public record Enemy(int id,
    *
    * @param type the EnemyType of the Enemy
    * @return the maximum health for an enemy of the given type
-   * @throws IllegalArgumentException if the given EnemyType isn't supported
+   * @throws NoSuchElementException if the given EnemyType isn't supported
    */
-  public int getMaxHealth(EnemyType type) throws IllegalArgumentException {
+  public int getMaxHealth(EnemyType type) throws NoSuchElementException {
     switch (type) {
       case SMALL -> {
         return 50;
@@ -46,7 +50,7 @@ public record Enemy(int id,
         return 150;
       }
       default -> {
-        throw new IllegalArgumentException();
+        throw new NoSuchElementException();
       }
     }
   }
@@ -56,9 +60,9 @@ public record Enemy(int id,
    *
    * @param type the EnemyType of the Enemy
    * @return the speed for an enemy of the given type
-   * @throws IllegalArgumentException if the given EnemyType isn't supported
+   * @throws NoSuchElementException if the given EnemyType isn't supported
    */
-  public int getSpeed(EnemyType type) throws IllegalArgumentException {
+  public int getSpeed(EnemyType type) throws NoSuchElementException {
     switch (type) {
       case SMALL -> {
         return 10;
@@ -70,7 +74,7 @@ public record Enemy(int id,
         return 5;
       }
       default -> {
-        throw new IllegalArgumentException();
+        throw new NoSuchElementException();
       }
     }
   }
@@ -80,9 +84,9 @@ public record Enemy(int id,
    *
    * @param type the EnemyType of the Enemy
    * @return the damage for an enemy of the given type
-   * @throws IllegalArgumentException if the given EnemyType isn't supported
+   * @throws NoSuchElementException if the given EnemyType isn't supported
    */
-  public int getDamage(EnemyType type) throws IllegalArgumentException {
+  public int getDamage(EnemyType type) throws NoSuchElementException {
     switch (type) {
       case SMALL -> {
         return 1;
@@ -94,7 +98,31 @@ public record Enemy(int id,
         return 3;
       }
       default -> {
-        throw new IllegalArgumentException();
+        throw new NoSuchElementException();
+      }
+    }
+  }
+
+  /**
+   * Returns the default money values for different enemy types.
+   *
+   * @param type the EnemyType of the Enemy
+   * @return the default money for an enemy of the given type
+   * @throws NoSuchElementException if the given EnemyType isn't supported
+   */
+  public int getMoney(EnemyType type) throws NoSuchElementException {
+    switch (type) {
+      case SMALL -> {
+        return 1;
+      }
+      case MEDIUM -> {
+        return 2;
+      }
+      case LARGE -> {
+        return 3;
+      }
+      default -> {
+        throw new NoSuchElementException();
       }
     }
   }
