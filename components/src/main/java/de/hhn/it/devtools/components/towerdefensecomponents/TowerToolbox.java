8494c4449290debd8d7c86624b0a5f290f8ec74a
Stefan Arsenovici <arsenovici@stud.hs-heilbronn.de>
2025-11-23 23:58:19 +0100

    added moneyMade

--- components/src/main/java/de/hhn/it/devtools/components/towerdefensecomponents/TowerToolbox.java
+++ components/src/main/java/de/hhn/it/devtools/components/towerdefensecomponents/TowerToolbox.java
@@ -15,16 +15,32 @@ public class TowerToolbox {
    * Attacks the enemy in range, that has advanced the furthest on the path.
    *
    * @param enemies as list of enemies
+   * @param towers as list of towers
    * @return {@code true} if attack was successful
    *         {@code false} otherwise
    * @throws IllegalArgumentException if towers or enemies do not exist.
    */
-  public ArrayList<Enemy> action(List<Tower> towers, List<Enemy> enemies) throws IllegalArgumentException {
+  public ArrayList<Enemy> action(List<Tower> towers, List<Enemy> enemies, EnemyToolbox enemyBox) throws IllegalArgumentException {
     return null;
   }
 
-  public int moneyMade(List<Tower> towers, List<Enemy> enemies){
-    return 0;
+
+  /**
+   * Attacks the enemy in range, that has advanced the furthest on the path.
+   *
+   * @param towers as list of towers
+   * @return {@code int} how much money was made
+   * @throws IllegalArgumentException if towers or enemies do not exist.
+   */
+  public int moneyMade(List<Tower> towers) {
+    int money = 0;
+    for (Tower tower : towers) {
+      if (tower.type().equals(TowerType.MONEYMAKER)) {
+        money += 50;
+      }
+    }
+
+    return money;
   }
 
 
