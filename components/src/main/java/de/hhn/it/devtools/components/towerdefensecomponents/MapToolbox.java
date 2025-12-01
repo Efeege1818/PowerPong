086581dcaba9d498bcb4821e44cb12c1b292bf65
Stefan Arsenovici <arsenovici@stud.hs-heilbronn.de>
2025-12-01 20:12:13 +0100

    addedMapToolbox

---
+++ components/src/main/java/de/hhn/it/devtools/components/towerdefensecomponents/MapToolbox.java
@@ -0,0 +1,30 @@
+package de.hhn.it.devtools.components.towerdefensecomponents;
+
+import de.hhn.it.devtools.apis.towerdefenseapi.Coordinates;
+import de.hhn.it.devtools.apis.towerdefenseapi.Direction;
+import de.hhn.it.devtools.apis.towerdefenseapi.Tower;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class MapToolbox {
+
+  public Grid generateMap(int size) {
+    return null;
+  }
+
+  public ArrayList<Coordinates> getPath(Grid grid) {
+    return null;
+  }
+
+  public boolean isTowerPlacementAllowed(Coordinates coordinates, List<Tower> towers, Grid grid) {
+    return false;
+  }
+
+
+
+
+
+}
