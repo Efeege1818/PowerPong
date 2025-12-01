c3db0340dc86ffe7a072038f7bda08e303a4d83a
Luis Mißbach <lmissbach@stud.hs-heilbronn.de>
2025-12-01 15:33:25 +0100

    Added LOCKED to WaveGenerator and SimpleWaveGenerator and fixed Chackstyle

--- components/src/main/java/de/hhn/it/devtools/components/towerdefensecomponents/SimpleWaveGenerator.java
+++ components/src/main/java/de/hhn/it/devtools/components/towerdefensecomponents/SimpleWaveGenerator.java
@@ -4,6 +4,7 @@ import de.hhn.it.devtools.apis.towerdefenseapi.Enemy;
 import java.util.HashMap;
 import java.util.Map;
 
+// LOCKED: L.Missbach
 public class SimpleWaveGenerator implements WaveGenerator {
   // saved data
   Map<Integer, Map<Integer, Enemy>> waveMap = new HashMap<>();
--- components/src/main/java/de/hhn/it/devtools/components/towerdefensecomponents/WaveGenerator.java
+++ components/src/main/java/de/hhn/it/devtools/components/towerdefensecomponents/WaveGenerator.java
@@ -1,35 +1,35 @@
 package de.hhn.it.devtools.components.towerdefensecomponents;
 
 import de.hhn.it.devtools.apis.towerdefenseapi.Enemy;
-
 import java.util.Map;
 
+// LOCKED: L.Missbach
 /**
  * Generator that generates the subsequent enemy waves.
  * Once generated it must save the enemies to a specific round.
  */
 public interface WaveGenerator {
 
-	// TODO: add methods
+  // TODO: add methods
 
-	/**
-	 * Calculates the total enemy power for a given wave.
-	 *
-	 * @param wave the wave number to calculate the power for
-	 * @return the corresponding amount of enemy power
-	 * @throws IllegalArgumentException if wave number is less than or equal to zero
-	 */
-	int getEnemyPower(int wave, int multiplier);
+  /**
+   * Calculates the total enemy power for a given wave.
+   *
+   * @param wave the wave number to calculate the power for
+   * @return the corresponding amount of enemy power
+   * @throws IllegalArgumentException if wave number is less than or equal to zero
+   */
+  int getEnemyPower(int wave, int multiplier);
 
-	/**
-	 * Generates a wave of enemies with the enemyPower.
-	 *
-	 * @param enemyPower will determine how strong the wave will be
-	 * @return Map filled with enemies and their id
-	 * @throws IllegalArgumentException if enemyPower number is less than or equal to zero
-	 */
-	Map<Integer, Enemy> generateWave(int enemyPower);
+  /**
+   * Generates a wave of enemies with the enemyPower.
+   *
+   * @param enemyPower will determine how strong the wave will be
+   * @return Map filled with enemies and their id
+   * @throws IllegalArgumentException if enemyPower number is less than or equal to zero
+   */
+  Map<Integer, Enemy> generateWave(int enemyPower);
 
-	// TODO: check logic and usefulness
-	Map<Integer, Map<Integer, Enemy>> wavePerRound(int round);
+  // TODO: check logic and usefulness
+  Map<Integer, Map<Integer, Enemy>> wavePerRound(int round);
 }
