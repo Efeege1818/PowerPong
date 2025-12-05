62ca24074f065e1a796db045d7e4433e7e0921b5
Luis Mißbach <lmissbach@stud.hs-heilbronn.de>
2025-12-05 17:56:03 +0100

    Fixed wrong merges

--- apis/src/main/java/de/hhn/it/devtools/apis/towerdefenseapi/Enemy.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/towerdefenseapi/Enemy.java
@@ -1,6 +1,5 @@
 package de.hhn.it.devtools.apis.towerdefenseapi;
 
-import java.util.NoSuchElementException;
 import java.util.UUID;
 
 /**
@@ -22,29 +21,5 @@ public record Enemy(UUID id,
                     Coordinates coordinates,
                     EnemyType type,
                     int currentHealth,
-<<<<<<< HEAD
-                    int index){}
-||||||| a2a5e14
                     int index
-) {
-
-  /**
-   * Returns the default maximum Heath values for different enemy types.
-   *
-   * @param type the EnemyType of the Enemy
-   * @return the maximum health for an enemy of the given type
-   * @throws NoSuchElementException if the given EnemyType isn't supported
-   */
-  public static int getMaxHealth(EnemyType type) throws NoSuchElementException {
-    return switch (type) {
-      case SMALL -> 50;
-      case MEDIUM -> 100;
-      case LARGE -> 150;
-      default -> throw new NoSuchElementException();
-    };
-  }
-}
-=======
-                    int index
-) {}
->>>>>>> 51f2b320b0b23910f28e7618af7ef7974d63f87d
+){}
