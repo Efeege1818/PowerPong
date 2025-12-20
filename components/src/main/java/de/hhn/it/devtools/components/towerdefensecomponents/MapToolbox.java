83fa8963abb3e983a62bf988bdc4329b0329000d
lalischer <lalischer@stud.hs-heilbronn.de>
2025-12-20 21:21:17 +0100

    implemented MapToolBox, except dijkstra

--- components/src/main/java/de/hhn/it/devtools/components/towerdefensecomponents/MapToolbox.java
+++ components/src/main/java/de/hhn/it/devtools/components/towerdefensecomponents/MapToolbox.java
@@ -1,56 +1,123 @@
 package de.hhn.it.devtools.components.towerdefensecomponents;
 
 import de.hhn.it.devtools.apis.towerdefenseapi.Coordinates;
+import de.hhn.it.devtools.apis.towerdefenseapi.Direction;
 import de.hhn.it.devtools.apis.towerdefenseapi.Grid;
-import de.hhn.it.devtools.apis.towerdefenseapi.Tower;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
-
 
 // LOCKED : L.Alischer
+
+/**
+ * Class for initiating all default map-related tasks, not including enemies and towers.
+ */
 public class MapToolbox {
 
-
-
-  /**
-   * Generates a Grid with the Dijkstra's algorithm.
-   *
-   * @param size of the Grid
-   * @return generated Grid
-   * @throws IllegalArgumentException if the given size is too small
-   */
-  public Grid generateMap(int size) {
-    return null;
-  }
-
+  Grid grid;
+  List<Coordinates> path;
+  int originSize;
 
   /**
-   * Returns the enemy's path of the given grid.
+   * Generates a bare Grid.
    *
-   * @param grid the grid on wich the path will be based on.
-   * @return the path of the grid
-   * @throws IllegalArgumentException if the given grid is null
+   * @param size of the Grid (must be positive)
    */
-  public ArrayList<Coordinates> getPath(Grid grid) {
-    return null;
+  public void generateMap(int size) {
+    this.originSize = size;
+    Direction[][] map = new Direction[size][size];
+
+    for (int row = 0; row < size; row++) {
+      for (int col = 0; col < size; col++) {
+        map[row][col] = Direction.NONE;
+      }
+    }
+    // Call for instantiating the path
+    generatePath();
+    this.grid = new Grid(map);
   }
 
   /**
-   * Returns the default money values for different enemy types.
+   * Getter for the already generated Grid.
    *
-   * @param coordinates where the tower should be placed
-   * @param towers is the list of towers already on the grid
-   * @param grid the grid on wich the tower will be placed on
-   * @return if the tower is allowed to be placed on the given coordinates
-   * @throws IllegalArgumentException if any given parameters are null
+   * @return the Grid
+   * @throws IllegalStateException when the Grid has not been generated yet.
    */
-  public boolean isTowerPlacementAllowed(Coordinates coordinates, List<Tower> towers, Grid grid) {
-    return false;
+  public Grid getGrid() throws IllegalStateException {
+    if (this.grid == null) {
+      throw new IllegalStateException("No grid has been generated.");
+    }
+    return this.grid;
   }
 
+  /**
+   * Returns the enemy's path of the generated grid.
+   *
+   * @return the path of the grid as a list of coordinates
+   * @throws IllegalArgumentException if no grid or path has been generated
+   */
+  public List<Coordinates> getPath() throws IllegalStateException {
+    if (this.path == null) {
+      throw new IllegalStateException("No path has been generated.");
+    }
+    return this.path;
+  }
 
+  /**
+   * Checks if a coordinate is a valid empty field to use.
+   *
+   * @param coordinates that is to be checked for possibility
+   * @return true if coordinate is an empty field, otherwise false
+   * @throws IllegalArgumentException if coordinates are not on the grid
+   */
+  public boolean isAllowed(Coordinates coordinates) throws IllegalStateException {
+    if (this.path == null || this.grid == null) {
+      throw new IllegalStateException("No path or grid has been generated.");
+    } else if (coordinates == null
+            || coordinates.x() > originSize
+            || coordinates.y() > originSize
+            || coordinates.x() < 0
+            || coordinates.y() < 0) {
+      throw new IllegalArgumentException("Invalid coordinates provided.");
+    }
 
+    return grid.grid()[(int) coordinates.x()][(int) coordinates.y()] == Direction.NONE;
+  }
 
+  /**
+   * Calculates the Euclidean (2-Dimensional) distance between two coordinate objects.
+   *
+   * @param coordinate1 first coordinate
+   * @param coordinate2 second coordinate
+   * @return the distance between the two coordinates as a double
+   * @throws IllegalArgumentException if one or both coordinates are invalid
+   */
+  public double calcDistance(Coordinates coordinate1, Coordinates coordinate2)
+          throws IllegalArgumentException {
+    if (coordinate1 == null || coordinate2 == null) {
+      throw new IllegalArgumentException("Coordinates must not be null.");
+    }
 
+    return calcDistance(coordinate1.x() - coordinate2.x(), coordinate1.y() - coordinate2.y());
+  }
+
+  /**
+   * Calculates the absolute distance between two float values.
+   *
+   * @param coordinate1 first value
+   * @param coordinate2 second value
+   * @return the absolute distance between the two values as a float
+   * @throws IllegalArgumentException if one or both values are invalid
+   */
+  public float calcDistance(float coordinate1, float coordinate2) throws IllegalArgumentException {
+
+    return Math.abs(coordinate1 - coordinate2);
+  }
+
+  /**
+   * Adds a path on the grid using an implementation of the Dijkstra's algorithm.
+   *
+   * @throws RuntimeException when something went wrong in the algorithm
+   */
+  private void generatePath() throws RuntimeException {
+    // TODO: implement dijkstra
+  }
 }
