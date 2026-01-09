5cdc9cde30d9e6207257448dfea570d1a673f5e1
lalischer <lalischer@stud.hs-heilbronn.de>
2026-01-09 15:13:41 +0100

    fixed & updated viewmodel for pairprogramming

--- javafx/src/main/java/de/hhn/it/devtools/javafx/towerDefense/viewModel/TowerDefenseViewModel.java
+++ javafx/src/main/java/de/hhn/it/devtools/javafx/towerDefense/viewModel/TowerDefenseViewModel.java
@@ -5,9 +5,7 @@ import de.hhn.it.devtools.apis.towerdefenseapi.Grid;
 import de.hhn.it.devtools.apis.towerdefenseapi.Player;
 import de.hhn.it.devtools.apis.towerdefenseapi.Tower;
 import de.hhn.it.devtools.apis.towerdefenseapi.TowerDefenseListener;
-import de.hhn.it.devtools.components.towerdefensecomponents.MapToolbox;
 import de.hhn.it.devtools.components.towerdefensecomponents.SimpleTowerDefenseService;
-import de.hhn.it.devtools.components.towerdefensecomponents.TowerToolbox;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ListProperty;
 import javafx.beans.property.ObjectProperty;
@@ -16,7 +14,6 @@ import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 
-import java.util.List;
 
 public class TowerDefenseViewModel implements TowerDefenseListener {
   SimpleTowerDefenseService service;
@@ -28,33 +25,35 @@ public class TowerDefenseViewModel implements TowerDefenseListener {
 
   public TowerDefenseViewModel(SimpleTowerDefenseService service) {
     this.service = service;
-
     this.service.addListener(this);
-
     map.set(service.getMap());
     round.set(service.getCurrentRound());
   }
 
   public void addTower(Tower tower) {
-    towers.add(tower);
     service.placeTower(tower);
+    sync();
   }
 
   public void startRound() {
     service.startGame();
     startNextRound();
+    sync();
   }
 
   public void abortGame() {
     service.abortGame();
+    sync();
   }
 
   public void resetGame() {
     service.resetGame();
+    sync();
   }
 
   public void startNextRound() {
     service.startNextRound();
+    sync();
   }
 
   public ObjectProperty<Grid> getMap() {
@@ -74,31 +73,38 @@ public class TowerDefenseViewModel implements TowerDefenseListener {
   }
 
   public ListProperty<Tower> getTowers() {
+    // TODO: set this to param {@Code towers}
+//    return new SimpleListProperty<>(FXCollections.observableArrayList(
+//            service.getTowerBoard().values()));
     return towers;
   }
 
   @Override
   public void updateHealth() {
-
+    sync();
   }
 
   @Override
   public void updateMoney() {
-
+    sync();
   }
 
   @Override
   public void gameEnded() {
-
+    sync();
   }
 
   @Override
   public void gameCompleted() {
-
+    sync();
   }
 
   @Override
   public void updateMap() {
+    sync();
+  }
 
+  public void sync() {
+    this.map.set(service.getMap());
   }
 }
