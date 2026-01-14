fd5427ab71326223236386f0c04263e3081a5193
Luis Mißbach <lmissbach@stud.hs-heilbronn.de>
2026-01-14 21:52:27 +0100

    Added Start via Main UI

--- javafx/src/main/java/de/hhn/it/devtools/javafx/controllers/TowerDefenseController.java
+++ javafx/src/main/java/de/hhn/it/devtools/javafx/controllers/TowerDefenseController.java
@@ -1,39 +1,29 @@
 package de.hhn.it.devtools.javafx.controllers;
 
-import de.hhn.it.devtools.javafx.controllers.template.PurProgrammingScreen;
-import de.hhn.it.devtools.javafx.controllers.template.ScreenController;
-import de.hhn.it.devtools.javafx.controllers.template.SingletonAttributeStore;
-import de.hhn.it.devtools.javafx.controllers.template.UnknownTransitionException;
 import de.hhn.it.devtools.javafx.towerdefense.controllers.TowerDefenseLauncher;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class TowerDefenseController extends Controller implements Initializable {
   private static final org.slf4j.Logger logger =
           org.slf4j.LoggerFactory.getLogger(TowerDefenseController.class);
-  public static final String SCREEN_CONTROLLER = "screen.controller";
 
   @FXML
   AnchorPane templateAnchorPane;
-  ScreenController screenController;
 
   public TowerDefenseController() {
     logger.debug("TowerDefense Controller created.");
   }
 
   @Override
-  public void initialize(final URL location, final ResourceBundle resources) {
-    // initialize screenController here because now we have the anchorPane.
-    screenController = new ScreenController(templateAnchorPane);
-
-
-  }
+  public void initialize(final URL location, final ResourceBundle resources) {}
 
   @FXML
   public void startGameButton() {
     logger.info("Starting towerDefense Game");
+    TowerDefenseLauncher.launch();
   }
 }
--- javafx/src/main/java/de/hhn/it/devtools/javafx/towerdefense/controllers/TowerDefenseLauncher.java
+++ javafx/src/main/java/de/hhn/it/devtools/javafx/towerdefense/controllers/TowerDefenseLauncher.java
@@ -3,42 +3,35 @@ package de.hhn.it.devtools.javafx.towerdefense.controllers;
 import de.hhn.it.devtools.apis.towerdefense.TowerDefenseService;
 import de.hhn.it.devtools.components.towerdefense.SimpleTowerDefenseService;
 import de.hhn.it.devtools.javafx.towerdefense.viewmodel.TowerDefenseViewModel;
-import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
  * Class that starts the TowerDefense Game.
  */
-public class TowerDefenseLauncher extends Application {
+public class TowerDefenseLauncher {
 
   TowerDefenseService service;
 
   /**
-   * The main method.
-   *
-   * @param args command line arguments
+   * Launches the Tower Defense Game.
    */
-  public static void main(String[] args) {
-
-    System.out.println("java version: " + System.getProperty("java.version"));
-    launch(args);
+  public static void launch() {
+    TowerDefenseLauncher launcher = new TowerDefenseLauncher();
+    launcher.start(new Stage());
   }
 
-  @Override
-  public void start(Stage primaryStage) {
+  private void start(Stage primaryStage) {
     service = new SimpleTowerDefenseService();
     TowerDefenseViewModel viewModel = new TowerDefenseViewModel(service);
     ScreenManager screenManager = new ScreenManager(viewModel);
     screenManager.switchTo(ScreenType.TITLE_SCREEN);
+    primaryStage.setOnHiding((event) -> {
+      service.terminate();
+    });
     primaryStage.setMaximized(true);
     primaryStage.setScene(new Scene(screenManager));
     primaryStage.setTitle("Tower Defense");
     primaryStage.show();
-  }
-
-  @Override
-  public void stop() {
-    service.terminate();
   }
 }
