574f2022c87b984e900c0db2f34d8fac391084d1
Anxhela Alimadhi <aanxhela@stud.hs-heilbronn.de>
2026-01-15 16:26:29 +0100

    Export fourconnect provider package

--- components/src/main/java/module-info.java
+++ components/src/main/java/module-info.java
@@ -1,13 +1,14 @@
 module devtools.components {
-  requires devtools.apis;
-  requires java.logging;
-  requires org.slf4j;
+    requires devtools.apis;
+    requires java.logging;
+    requires org.slf4j;
 
-  exports de.hhn.it.devtools.components.example.coffeemakerservice.provider;
+    exports de.hhn.it.devtools.components.example.coffeemakerservice.provider;
+    exports de.hhn.it.devtools.components.fourconnect.provider;
 
-  provides de.hhn.it.devtools.apis.examples.coffeemakerservice.CoffeeMakerService
-      with de.hhn.it.devtools.components.example.coffeemakerservice.provider.WnckCoffeeMakerService;
+    provides de.hhn.it.devtools.apis.examples.coffeemakerservice.CoffeeMakerService
+            with de.hhn.it.devtools.components.example.coffeemakerservice.provider.WnckCoffeeMakerService;
 
-  provides de.hhn.it.devtools.apis.examples.coffeemakerservice.AdminCoffeeMakerService
-      with de.hhn.it.devtools.components.example.coffeemakerservice.provider.WnckCoffeeMakerService;
+    provides de.hhn.it.devtools.apis.examples.coffeemakerservice.AdminCoffeeMakerService
+            with de.hhn.it.devtools.components.example.coffeemakerservice.provider.WnckCoffeeMakerService;
 }
